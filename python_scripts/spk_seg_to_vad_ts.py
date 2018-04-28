98fcdc75eb0179ca52bdfc34e6760ae3e0bfca32
rabbeh <rajathebbar@gmail.com>
2018-04-27 17:09:59 -0700

    included speaker-segmentation

---
+++ python_scripts/spk_seg_to_vad_ts.py
@@ -0,0 +1,22 @@
+import os, sys
+
+movie, expt_dir = sys.argv[1:]
+VAD_ts_file = os.path.join(expt_dir, 'VAD/timestamps/', movie + '_wo_ss.ts')
+spk_seg_file = os.path.join(expt_dir, 'VAD/spk_seg/', movie + '.seg')
+
+spk_seg_data = [x.rstrip().split() for x in open(spk_seg_file,'r').readlines()]
+spk_seg_times = [[x[1], float(x[2]), float(x[3])] for x in spk_seg_data]
+vad_data = [x.rstrip().split() for x in open(VAD_ts_file,'r').readlines()]
+vad_times = [[float(x[0]), float(x[1])] for x in vad_data]
+fw = open(os.path.join(expt_dir, 'VAD/timestamps/', movie + '.ts'),'w')
+
+for seg in spk_seg_times:
+    vad_num = int(seg[0].split('_vad-')[1])-1
+#    print(vad_num)
+    start = vad_times[vad_num][0] + seg[1]
+    end = vad_times[vad_num][0] + seg[2]
+    fw.write('{} {}\n'.format(start, end))
+
+fw.close()
+    
+    
