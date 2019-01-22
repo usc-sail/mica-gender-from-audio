fbbd8f97ea1e0430543f85d824c59051456ca039
rajatheb <rajatheb@usc.edu>
2019-01-22 13:18:12 -0800

    Added checks

--- bash_scripts/create_wav_files.sh
+++ bash_scripts/create_wav_files.sh
@@ -1,11 +1,11 @@
 ##
 ##
-##      Extract audio (mono,sampled at 8kHz) from given AV files
+##      Extract audio (mono,sampled at 16kHz) from given AV files
 ##
 ##
 ##      Arguments :
 ##      movie_paths :  List of paths to input files to process
-##      wav_dir     :  Directory in which to store complete audio files 
+##      wav_dir     :  Directory in which to store audio files 
 ##      nj          :  Number of parallel jobs to process
 ##
 ##
@@ -22,7 +22,7 @@ do
     base=`basename -- $mov_file`
     movie_name=`echo $base | awk -F '.' '{ print $1 }'` 
 
-    ffmpeg -loglevel panic -i ${mov_file} -ar 8k -ac 1 ${wav_dir}/${movie_name}.wav &  ## Extract single-channel audio from input sampled at 8000 Hz.
+    ffmpeg -loglevel error -i ${mov_file} -ar 16k -ac 1 ${wav_dir}/${movie_name}.wav & ## Extract single-channel audio from input sampled at 16000 Hz.
     if [ $(($movie_num % $nj )) -eq 0 ]
     then
         wait
@@ -30,3 +30,12 @@ do
     movie_num=`expr $movie_num + 1`
 done
 wait
+
+for mov_file in `cat ${movie_paths}`
+do
+    base=`basename -- $mov_file`
+    movie_name=`echo $base | awk -F '.' '{ print $1 }'` 
+    if [ ! -e ${wav_dir}/${movie_name}.wav ]; then
+        echo "Unable to extract ${mov_file}, please check if corrupted file"
+    fi
+done
