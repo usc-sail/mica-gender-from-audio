fa1c9788ee2cd88ec9613caf66d2617a9202fc5f
Rajat Hebbar <rajathebbar@gmail.com>
2019-08-24 16:30:29 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,4 +2,4 @@ feats_flag="y"      # "y"/"n" flag to keep kaldi-feature files after inference
 wavs_flag="n"       # "y"/"n" flag to keep .wav audio files after inference
 nj=4                # Number of parallel processes for kaldi steps
 vad_overlap=0       # % overlap in VAD-segments (range: 0-1, 0 for no overlap) (single segment is 0.64s)
-gender_overlap=0.33    # % overlap in GENDER-segment 
+gender_overlap=0    # % overlap in GENDER-segment 
