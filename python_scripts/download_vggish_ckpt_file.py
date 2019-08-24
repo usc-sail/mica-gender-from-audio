8b89ef3182442c16e9b199a85baf1c822a4136fe
rajatheb <rajatheb@usc.edu>
2019-08-24 15:54:32 -0700

    Added comments

--- python_scripts/download_vggish_ckpt_file.py
+++ python_scripts/download_vggish_ckpt_file.py
@@ -1,5 +1,24 @@
+'''
+Author/year: Rajat Hebbar, 2019
+
+Download checkpoint file for extracting VGGish embeddings
+for gender ID prediction. This will be executed only if the
+.ckpt file is not found in proj_dir/python_scripts/audio_scripts/
+
+Input
+    1) checkpoint : Path to .ckpt file 
+
+Output
+    1) checkpoint-file : Downloaded .ckpt file
+
+Usage
+    python download_vggish_ckpt_file.py checkpoint
+
+Example
+    python download_vggish_ckpt_file.py python_scripts/audioset_scripts/vggish_model.ckpt 
+'''
+
 import os, sys, requests
-checkpoint=sys.argv[1]
 
 def download_file_from_google_drive(file_id, destination):
     def get_confirm_token(response):
@@ -30,4 +49,5 @@ def main():
         download_file_from_google_drive('1c-wi6F_Fv0Z0TmJBpSrlTT0iCDmKF_NJ', checkpoint)
 
 if __name__=='__main__':
+    checkpoint=sys.argv[1]
     main()
