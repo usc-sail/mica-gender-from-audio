7d4f7c718cee8b59b9554c98c983717d2cbb89e0
rajatheb <rajatheb@usc.edu>
2019-08-25 16:15:27 -0700

    added config for segmentation

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
+nj=4                # Number of files to process simultaneously
 feats_flag="y"      # "y"/"n" flag to keep kaldi-feature files after inference
 wavs_flag="n"       # "y"/"n" flag to keep .wav audio files after inference
-nj=4                # Number of parallel processes for kaldi steps
 vad_overlap=0       # % overlap in VAD-segments (range: 0-1, 0 for no overlap) (single segment is 0.64s)
 gender_overlap=0    # % overlap in GENDER-segment 
+uniform_seg_len=2.0   # Segment length for uniform speaker-segmentation 
