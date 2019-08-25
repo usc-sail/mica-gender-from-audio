9dd8f65b4647d0df7c80a87c18c37bb964e2889e
rajatheb <rajatheb@usc.edu>
2019-08-25 16:18:15 -0700

    Included uniform segmentation of SAD segments

--- python_scripts/generate_vad_labels.py
+++ python_scripts/generate_vad_labels.py
@@ -41,10 +41,8 @@ import warnings
 #warnings.filterwarnings("ignore")
 
 
-##
 ##  Convert frame-level posteriors into 
 ##  continuous segments of regions where post=pos_label
-##
 def frame2seg(frames, frame_time_sec=0.01, pos_label=1):
     pos_idxs = np.where(frames==pos_label)[0]
     pos_regions = np.split(pos_idxs, np.where(np.diff(pos_idxs)!=1)[0]+1)
@@ -53,12 +51,52 @@ def frame2seg(frames, frame_time_sec=0.01, pos_label=1):
     segments = np.array([[x[0], x[-1]+1] for x in pos_regions])*frame_time_sec
     return segments
 
+## Mean and variance normalization for features
 def normalize(data):
     return np.divide(np.subtract(data, np.mean(data)), np.std(data))
 
+
+## Uniform segmentation of VAD segments
+def perform_uniform_segmentation(movie, segments, write_dir, max_segment_duration=2, min_remaining_duration=0.64):
+    write_file = os.path.join(write_dir, movie + '_subsegments.ts')
+    subseg_fp = open(write_file, 'w')
+    dur_threshold = max_segment_duration + min_remaining_duration
+
+    for seg_num, seg in enumerate(segments):
+        utt_id = '{movie}_vad-{seg_num:04d}'.format(movie=movie, seg_num=seg_num)
+        start_time = seg[0]
+        end_time = seg[1]
+
+        dur = end_time - start_time
+
+        start = start_time
+        while (dur > dur_threshold):
+            end = start + max_segment_duration
+            start_relative = start - start_time
+            end_relative = end - start_time
+            new_utt = "{utt_id}-{s:08d}-{e:08d}".format(
+                utt_id=utt_id, s=int(100 * start_relative),
+                e=int(100 * end_relative))
+            subseg_fp.write("{new_utt} {utt_id} {s:.3f} {e:.3f}\n".format(
+                new_utt=new_utt, utt_id=utt_id, s=start,
+                e=start + max_segment_duration))
+            start += max_segment_duration 
+            dur -= max_segment_duration
+
+        end = end_time
+        new_utt = "{utt_id}-{s:08d}-{e:08d}".format(
+            utt_id=utt_id, s=int(round(100 * (start - start_time))),
+            e=int(round(100 * (end - start_time))))
+        subseg_fp.write("{new_utt} {utt_id} {s:.3f} {e:.3f}\n".format(
+            new_utt=new_utt, utt_id=utt_id, s=start,
+            e=end))
+    subseg_fp.close()        
+
+
 def main():
-    expt_dir, feats_scp, model_file = sys.argv[1:-1]
-    overlap = float(sys.argv[-1])
+    expt_dir, feats_scp, model_file = sys.argv[1:-2]
+    overlap = float(sys.argv[-2])
+    uniform_seg_len = float(sys.argv[-1])
     assert overlap >=0 and overlap <1, "Invalid choice of overlap, should be 0 <= overlap < 1"
     num_frames, num_freq_bins = (64, 64)
     fps = 100
@@ -86,27 +124,17 @@ def main():
         # Post-processing of posteriors
         labels = np.round(predictions)
         seg_times = frame2seg(np.round(labels))
-        
-        # Write start and end VAD timestamps 
-        fw = open(os.path.join(write_ts, movie + '_wo_ss.ts'),'w')
-        if not os.path.exists(os.path.join(vad_wav_dir,movie)):
-            os.makedirs(os.path.join(vad_wav_dir, movie))
+        perform_uniform_segmentation(movie, seg_times, write_ts, max_segment_duration=uniform_seg_len, min_remaining_duration=0.64)
 
-        seg_ct = 1
-        for segment in seg_times:
-        # Threshold to 50ms minimum segment duration
-            if segment[1]-segment[0] > 0.05:
-                fw.write('{0}_vad-{1:04} {0} {2:0.2f} {3:0.2f}\n'.format(movie, seg_ct, segment[0], segment[1]))
-                seg_ct += 1
-        fw.close()
+        # Write start and end VAD timestamps 
+        with open(os.path.join(write_ts, movie + '.ts'),'w') as ts_fp:
+            for seg_num, segment in enumerate(seg_times):
+                ts_fp.write('{0}_vad-{1:04} {0} {2:0.2f} {3:0.2f}\n'.format(movie, seg_num, segment[0], segment[1]))
 
         # Write frame-level posterior probabilities
-        fpost = open(os.path.join(write_post, movie + '.post'),'w')
-        for frame in predictions:
-            fpost.write('{0:0.2f}\n'.format(frame))
-        fpost.close()
-
-        fw.close()
+        with open(os.path.join(write_post, movie + '.post'),'w') as post_fp:
+            for frame in predictions:
+                post_fp.write('{0:0.2f}\n'.format(frame))
 
 if __name__ == '__main__':
     main()
